6be3c9e88bd5156054b09398d71e4d0e68a484bd
Gabe Nunez <gnunez@prosofteng.com>
2018-07-25 13:24:00 -0700

    Add controlled component input

---
+++ src/containers/search_bar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class Searchbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''};
+
+        // Binding the onInputChange function to the correct context.
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    render() {
+        return (
+            <form className="input-group">
+                <input 
+                    placeholder="Get a give-day forecast"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        )
+    }
+}
